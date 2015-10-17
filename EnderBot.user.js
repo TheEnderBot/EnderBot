407f83fadaf6b82f3d60c0feec8b957057297c04
Enderbel <belenko1998@yandex.ru>
2015-10-17 13:32:27 +0300

    Update EnderBot.user.js

--- EnderBot.user.js
+++ EnderBot.user.js
@@ -1,20 +1,19 @@
-/*The MIT License (MIT)
-Copyright (c) 2015 Apostolique
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.*/
+/*
+Copyright (c) 2015 EnderBel
+
+Данный бот был разработан дабы наказать raga.pw за их высокие цены =D
+
+г==¬-г¬г¬г¬г¬г¬--г¬г==¬г¬г¬г¬г¬г===¬г¬
+¦г¬¦-¦¦¦¦¦¦¦¦¦¦--¦¦¦г¬¦¦¦¦¦¦¦¦¦¦г==-¦¦
+¦L-L¬¦L-¦¦¦¦¦¦L¬г-¦¦L-¦¦L-¦¦¦¦¦¦L==¬¦¦
+¦г=¬¦¦г¬¦¦¦г¦¦г¬г¬¦¦г¬¦¦г¬¦¦¦г¦¦г==-L-
+¦L=-¦¦¦¦¦¦L-¦¦¦L-¦¦¦¦¦¦¦¦¦¦¦L-¦¦L==¬г¬
+L===-L-L-L==-L---L-L-L-L-L-L==-L===-L-
+
+Запрещено изменять-распространять данный код без моего разрешения.
+
+
+*/
 
 // ==UserScript==
 // @name EnderBot
@@ -22,7 +21,7 @@ SOFTWARE.*/
 // @include http://agar.io/*
 // @version 1.0
 // @grant none
-// @author http://www.twitch.tv/apostolique
+// @author EnderBel Skype:Ytrioq
 // @require http://www.parsecdn.com/js/parse-1.5.0.min.js
 // ==/UserScript==
 
